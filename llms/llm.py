e70f99730dfb4890adef2ec660d7202955c4b553
Mukund Ravi Iyer <mukiyer21@gmail.com>
2025-12-16 15:17:36 +0530

    ddqqd

--- llms/llm.py
+++ llms/llm.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 llm = ChatGroq(
-    model_name="llama-3.1-70b-versatile",
+    model_name="llama-3.3-70b-versatile",
     temperature=0.5,
     api_key=os.getenv("GROQ_API_KEY")
 )
